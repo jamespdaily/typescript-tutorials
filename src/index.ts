256dde17ea41109357e8e84925e7d48d30d20ec0
Jim Daily <jaypeedaylee@gmail.com>
2021-11-29 00:01:48 -0500

    lint updates

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,52 @@
-let id: number = 5;
-let company: string = 'Daily Dump LLC';
-let isPublished: boolean = true;
-let x: any = 'Hello';
+const id = 5;
+const company = 'Daily Dump LLC';
+const isPublished = true;
+const x: any = 'Hello';
 let age: number;
 
-let ids: number[] = [1, 2, 3, 4, 5];
-let arr: any[] = [1, true, 'Fuck'];
+const ids: number[] = [1, 2, 3, 4, 5];
+const arr: any[] = [1, true, 'Fuck'];
 
 // Tuple
-let person: [number, string, boolean] = [1, 'Jim', true];
+const person: [number, string, boolean] = [1, 'Jim', true];
 
 // Tuple array
 let employee: [number, string][];
 
 employee = [
-    [1, 'Jim'],
-    [2, 'Katie'],
-    [3, 'Nora'],
+  [1, 'Jim'],
+  [2, 'Katie'],
+  [3, 'Nora'],
 ];
 
 // Union
-let pid: string | number = 22;
+const pid: string | number = 22;
 
 // Enum
 enum Direction1 {
-    Up,
-    Down,
-    Left,
-    Right,
+  Up,
+  Down,
+  Left,
+  Right,
 }
 
 // Objects
 type User = {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 };
 
 const user: User = {
-    id: 1,
-    name: 'Jim',
+  id: 1,
+  name: 'Jim',
 };
 
 // Type Assertion
-let cid: any = 1;
+const cid: any = 1;
 // let customerId = <number>cid;
-let customerId = cid as number;
+const customerId = cid as number;
 
 // Functions
 function addNum(x, y) {
-    return x + y;
+  return x + y;
 }
